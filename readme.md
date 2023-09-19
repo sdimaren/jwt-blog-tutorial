3224fd824f281dcfe41b97321d7e67b9f9fd1d73
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-09-19 12:41:19 -0400

    jwt auth implemented, fully working

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ## Express Template
+
+[Alex Merced Blog Tutorial](https://tuts.alexmercedcoder.dev/2021/11/Auth_with_express_JWT/)
--- utils/middleware.js
+++ utils/middleware.js
@@ -3,24 +3,33 @@ const cookieParser = require("cookie-parser")
 const morgan = require("morgan")
 const cors = require("cors")
 const corsOptions = require("./cors")
+// import controllers
 const HomeController = require("../controllers/HomeController")
+const AuthController = require("../controllers/AuthController")
+const TodoController = require("../controllers/TodoController")
+// import models
+const User = require("../models/user")
+const Todo = require("../models/Todo")
 
 // function to create context property in every request with shared data
 const applicationContext = (req, res, next) => {
-    // data to share can be added in this object
-    req.context = {}
-    // move on to next middleware
-    next()
+  // data to share can be added in this object
+  req.context = {
+    models: { User, Todo },
+  }
+  // move on to next middleware
+  next()
 }
 
-
-const registerMiddleware = (app) => {
-    app.use(cors(corsOptions)) // cors headers
-    app.use(cookieParser()) // parse cookies
-    app.use(express.json()) // parse json bodies
-    app.use(morgan("tiny")) // logging
-    app.use(applicationContext) // add context object to request
-    app.use("/", HomeController) // register homecontroller routes for  "/" urls
+const registerMiddleware = app => {
+  app.use(cors(corsOptions)) // cors headers
+  app.use(cookieParser()) // parse cookies
+  app.use(express.json()) // parse json bodies
+  app.use(morgan("tiny")) // logging
+  app.use(applicationContext) // add context object to request
+  app.use("/", HomeController) // register homecontroller routes for  "/" urls
+  app.use("/auth", AuthController) // register homecontroller routes for  "/auth" urls
+  app.use("/todo", TodoController) // register todocontroller routes for  "/todo" urls
 }
 
 module.exports = registerMiddleware
