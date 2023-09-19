796995702b85436460aeb92e7fd13695bb2cbab2
Santiago Dimaren <sdimaren1@gmail.com>
2023-09-19 13:45:58 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,60 @@
-const app = require('express')();
+require("dotenv").config() // load variables from .env
+const express = require("express")
+const registerMiddleware = require("./utils/middleware")
 const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const morgan = require("morgan")
+const cors = require("cors")
+const corsOptions = require("./cors")
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+// Grab any ENV variables to be used, set default values in case .env file missing
+const { PORT = 3000 } = process.env
+
+// The Application Object
+const app = express()
+
+// registerMiddleware
+registerMiddleware(app)
+
+// Server listener
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+
+
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const morgan = require("morgan")
+const cors = require("cors")
+const corsOptions = require("./cors")
+// import controllers
+const HomeController = require("../controllers/HomeController")
+const AuthController = require("../controllers/AuthController")
+const TodoController = require("../controllers/TodoController")
+// import models
+const User = require("../models/user")
+const Todo = require("../models/Todo")
+
+// function to create context property in every request with shared data
+const applicationContext = (req, res, next) => {
+  // data to share can be added in this object
+  req.context = {
+    models: { User, Todo },
+  }
+  // move on to next middleware
+  next()
+}
+
+const registerMiddleware = app => {
+  app.use(cors(corsOptions)) // cors headers
+  app.use(cookieParser()) // parse cookies
+  app.use(express.json()) // parse json bodies
+  app.use(morgan("tiny")) // logging
+  app.use(applicationContext) // add context object to request
+  app.use("/api", HomeController) // register homecontroller routes for  "/" urls
+  app.use("/api/auth", AuthController) // register homecontroller routes for  "/auth" urls
+  app.use("/api/todo", TodoController) // register todocontroller routes for  "/todo" urls
+}
 
 module.exports = app;
